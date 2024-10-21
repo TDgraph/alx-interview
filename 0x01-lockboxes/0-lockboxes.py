d90ac3f9bda6bfa95afd800af47c129897184283
Temitope Douglas <taymeedouglas07@gmail.com>
2024-10-21 21:29:08 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lock boxes"""
+
+
+def can_unlock_all(boxes):
+    """Check if all of boxes can be opened"""
+    num_boxes = len(boxes)
+    keys = set()
+    for idx, box in enumerate(boxes, start=1):
+        for key in box:
+            if key < num_boxes:
+                keys.add(key)
+                keys.update(boxes[key])
+            if len(keys) == num_boxes-1 or len(keys) == num_boxes:
+                return True
+        if idx not in keys:
+            return False
+
+
+canUnlockAll = can_unlock_all
+
